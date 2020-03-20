936cf78b4f067de0ceec5dc3cfa8041fe4768aed
Jim Lloyd <jim.lloyd@gmail.com>
2020-03-19 20:32:06 -0700

    Remove option parsing

--- webSocketServer.cpp
+++ webSocketServer.cpp
@@ -7,10 +7,6 @@
 #include "helpers/AsyncFileStreamer.h"
 #include "helpers/Middleware.h"
 
-/* optparse */
-#define OPTPARSE_IMPLEMENTATION
-#include "uWebSockets/examples/helpers/optparse.h"
-
 enum CommandCodes : char
 {
     DEALCARD = 0,
@@ -69,54 +65,17 @@ void playCard(Socket* ws, char card, char seat)
     sendMessage(ws, message);
 }
 
-int main(int argc, char **argv) {
-
-    int option;
-    struct optparse options;
-    optparse_init(&options, argv);
-
-    struct optparse_long longopts[] = {
-        {"port", 'p', OPTPARSE_REQUIRED},
-        {"help", 'h', OPTPARSE_NONE},
-        {"passphrase", 'a', OPTPARSE_REQUIRED},
-        {"key", 'k', OPTPARSE_REQUIRED},
-        {"cert", 'c', OPTPARSE_REQUIRED},
-        {"dh_params", 'd', OPTPARSE_REQUIRED},
-        {0}
-    };
-
+int main(int argc, char **argv)
+{
+    // For now, we use a hard-coded port
     int port = 3001;
-    struct us_socket_context_options_t ssl_options = {};
 
-    while ((option = optparse_long(&options, longopts, nullptr)) != -1) {
-        switch (option) {
-        case 'p':
-            port = atoi(options.optarg);
-            break;
-        case 'a':
-            ssl_options.passphrase = options.optarg;
-            break;
-        case 'c':
-            ssl_options.cert_file_name = options.optarg;
-            break;
-        case 'k':
-            ssl_options.key_file_name = options.optarg;
-            break;
-        case 'd':
-            ssl_options.dh_params_file_name = options.optarg;
-            break;
-        case 'h':
-        case '?':
-            fail:
-            std::cout << "Usage: " << argv[0] << " [--help] [--port <port>] [--key <ssl key>] [--cert <ssl cert>] [--passphrase <ssl key passphrase>] [--dh_params <ssl dh params file>] <public root>" << std::endl;
-            return 0;
-        }
+    if (argc <= 1)
+    {
+        std::cerr << "Must provide one argument: the path to the root directory\n";
+        exit(1);
     }
-
-    char *root = optparse_arg(&options);
-    if (!root) {
-        goto fail;
-    }
+    char *root = argv[1];
 
     AsyncFileStreamer asyncFileStreamer(root);
 
